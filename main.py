bc9bfcd142bf83d7042c97a41912dd83ae3a9fcb
Kutz <100941428+Kutz-Dag@users.noreply.github.com>
2024-10-07 14:05:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-# adding code soon
+import copy
+import random
+
+class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
+    
+    def draw(self, num_balls):
+        if num_balls >= len(self.contents):
+            drawn_balls = self.contents.copy()
+            self.contents = []
+            return drawn_balls
+        
+        drawn_balls = random.sample(self.contents, num_balls)
+        
+        for ball in drawn_balls:
+            self.contents.remove(ball)
+        
+        return drawn_balls
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    successful_experiments = 0
+    
+    for _ in range(num_experiments):
+        
+        hat_copy = copy.deepcopy(hat)
+        
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+        
+        drawn_counts = {color: drawn_balls.count(color) for color in expected_balls}
+
+        success = all(drawn_counts.get(color, 0) >= count for color, count in expected_balls.items())
+        
+        if success:
+            successful_experiments += 1
+
+    return successful_experiments / num_experiments
+
+hat = Hat(black=6, red=4, green=3)
+probability = experiment(hat=hat,
+                         expected_balls={'red': 2, 'green': 1},
+                         num_balls_drawn=5,
+                         num_experiments=2000)
+print(probability)
